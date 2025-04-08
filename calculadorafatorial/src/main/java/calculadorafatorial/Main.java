6564507b66c3c8885d85d6442be879eb034a7037
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-08 15:31:46 -0300

    Terceiro Commit

--- calculadorafatorial/src/main/java/calculadorafatorial/Main.java
+++ calculadorafatorial/src/main/java/calculadorafatorial/Main.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int numero;
+        int i;
+        int numero1;
+
+
         Scanner input = new Scanner(System.in);
+        System.out.println("Insira um número: ");
         numero = input.nextInt();
 
-
+        numero1 = numero;
+        for (i = numero - 1; i >= 1; i--)
+        {
+            numero1 = numero1*i;
+        }
         
 
     }
