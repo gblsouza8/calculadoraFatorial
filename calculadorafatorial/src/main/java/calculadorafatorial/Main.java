1339477de4d32be29ba58e51a058aafdf2d899c4
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-08 15:25:28 -0300

    Segundo Commit

--- calculadorafatorial/src/main/java/calculadorafatorial/Main.java
+++ calculadorafatorial/src/main/java/calculadorafatorial/Main.java
@@ -1,7 +1,14 @@
 package calculadorafatorial;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero;
+        Scanner input = new Scanner(System.in);
+        numero = input.nextInt();
+
+
+        
+
     }
 }
